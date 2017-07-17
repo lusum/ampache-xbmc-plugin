0b01de52a26ba06251945503401ee8e9904b2df8
lusum <lusum@users.sourceforge.net>
2017-07-17 12:45:53 +0200

    add persisten connection speed up info download part 2

--- default.py
+++ default.py
@@ -259,9 +259,9 @@ def ampache_http_request(action,add=None, filter=None, limit=5000, offset=0):
             ssl_certs_str = ampache.getSetting("disable_ssl_certs")
             if str_to_bool(ssl_certs_str):
                 gcontext = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
-                response = urllib2.urlopen(req, context=gcontext)
+                response = urllib2.urlopen(req, context=gcontext, timeout=100)
             else:
-                response = urllib2.urlopen(req)
+                response = urllib2.urlopen(req, timeout=100)
             contents = response.read()
             tree=ET.XML(contents)
             response.close()
